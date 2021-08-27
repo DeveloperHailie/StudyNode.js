a6c95861e2ce3645dc78c97c5c13cdc1191c01bd
hailie <20181011@sungshin.ac.kr>
2021-08-27 19:17:51 +0900

    단위테스트: 추가 요구사항 반영하여 수정

--- 심화반/HELLO-JEST/validation.js
+++ 심화반/HELLO-JEST/validation.js
@@ -1,15 +1,49 @@
 module.exports = {
     isEmail: (value) => {
-        if(value.split('@').length !== 2){ // aa@bb이면 aa,bb 겠지
+        const [localPart, domain, ...etc] = value.split('@');
+
+        if (!localPart || !domain || etc.length) {
             return false;
         }
-        if(value.includes(' ')){
+        if (value.includes(' ')) {
             return false;
         }
-        if(value[0]==='-'){
+        if (value[0] === '-') {
             return false;
         }
-            
+
+        for (const word of localPart) {
+            if ('a' <= word && word <= 'z') {
+                continue;
+            }
+            if ('A' <= word && word <= 'Z') {
+                continue;
+            }
+            if ('0' <= word && word <= '9') {
+                continue;
+            }
+            if (word === '+' || word === '-' || word === '_') {
+                continue;
+            }
+            return false;
+        }
+
+        for (const word of domain) {
+            if ('a' <= word && word <= 'z') {
+                continue;
+            }
+            if ('A' <= word && word <= 'Z') {
+                continue;
+            }
+            if ('0' <= word && word <= '9') {
+                continue;
+            }
+            if (word === '.' || word === '-') {
+                continue;
+            }
+            return false;
+        }
+
         return true;
     },
 };
