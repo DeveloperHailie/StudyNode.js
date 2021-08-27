29d97da13c21b0b6d678edf8acff864242e9f4fa
hailie <20181011@sungshin.ac.kr>
2021-08-27 18:51:35 +0900

    단위테스트위한 validation.js 수정

--- 심화반/HELLO-JEST/validation.js
+++ 심화반/HELLO-JEST/validation.js
@@ -1,30 +1,15 @@
 module.exports = {
     isEmail: (value) => {
-        let emailValidation = true;
-        let hasAtSign = false;
-
-        if (value[0]=="-") // 맨 앞글자 - 체크
-            emailValidation = false;
-        for(let char of value){
-            if(!emailValidation){
-                continue;
-            }
-            if(char == " "){ // 공백 존재 체크
-                emailValidation = false;
-            }
-            if(char == "@") { // @ 체크
-                if(hasAtSign){ // @가 이미 존재하는 경우
-                    emailValidation = false;
-                }
-                else{
-                    hasAtSign = true;
-                }
-            }
+        if(value.split('@').length !== 2){ // aa@bb이면 aa,bb 겠지
+            return false;
         }
-        if(!hasAtSign){ // @ 1개도 없는 경우
-            emailValidation = false;
+        if(value.includes(' ')){
+            return false;
+        }
+        if(value[0]==='-'){
+            return false;
         }
             
-        return emailValidation;
+        return true;
     },
 };
