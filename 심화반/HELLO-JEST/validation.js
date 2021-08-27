838d8b019eabeee41c158d87a19b3fe277245f61
hailie <20181011@sungshin.ac.kr>
2021-08-27 19:27:04 +0900

    이메일 검증 방법 변경 - 정규식

--- 심화반/HELLO-JEST/validation.js
+++ 심화반/HELLO-JEST/validation.js
@@ -1,39 +1,17 @@
 module.exports = {
     isEmail: (value) => {
-        const [localPart, domain, ...etc] = value.split('@');
+        const email = value || '';
+        const [localPart, domain, ...etc] = email.split('@');
 
         if (!localPart || !domain || etc.length) {
             return false;
-        }
-        if (value.includes(' ')) {
+        } else if (email.includes(' ')) {
             return false;
-        }
-        if (value[0] === '-') {
+        } else if (email[0] === '-') {
             return false;
-        }
-
-        for (const word of localPart) {
-            if (!/^[0-9a-z+_-]+$/gi.test(word)){
-                return false;
-            }
-        }
-
-        for (const word of domain) {
-            const allowedSpecialWords = ['.','-'];
-            if(allowedSpecialWords.includes(word)){
-                continue;
-            }
-
-            if ('a' <= word && word <= 'z') {
-                continue;
-            }
-            if ('A' <= word && word <= 'Z') {
-                continue;
-            }
-            if ('0' <= word && word <= '9') {
-                continue;
-            }
-           
+        } else if (!/^[a-z0-9+_-]+$/gi.test(localPart)) {
+            return false;
+        } else if (!/^[a-z0-9.-]+$/gi.test(domain)) {
             return false;
         }
 
