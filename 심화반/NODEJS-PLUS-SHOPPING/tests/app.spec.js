5c35e20f8d554d37adc9dd8c026deb8453a3c9d9
hailie <20181011@sungshin.ac.kr>
2021-09-02 19:50:58 +0900

    describe-test로 변경

--- 심화반/NODEJS-PLUS-SHOPPING/tests/app.spec.js
+++ 심화반/NODEJS-PLUS-SHOPPING/tests/app.spec.js
@@ -1,25 +1,22 @@
 const app = require('../app');
-const supertest = require("supertest");
+const supertest = require('supertest');
 
-test("/index.html 경로에 요청했을 때 status code가 200이어야 한다.", async () => { 
-    const res = await supertest(app).get("/index.html");
-    expect(res.status).toEqual(200);
+describe('html을 반환하는 api',()=>{
+    test('/index.html 경로에 요청했을 때 status code가 200이어야 한다.', async () => {
+        const res = await supertest(app).get('/index.html');
+        expect(res.status).toEqual(200);
+    });
+    
+    test('/test.html 경로에 요청했을 때 status code가 404이어야 한다.', async () => {
+        const res = await supertest(app).get('/test.html');
+        expect(res.status).toEqual(404);
+    });
 });
 
-test("/test.html 경로에 요청했을 때 status code가 404이어야 한다.", async () => { 
-    const res = await supertest(app).get("/test.html");
-    expect(res.status).toEqual(404);
-});
 
-/*
-test("/api/goods/add/crawling 경로에 요청했을 때 status code가 200이어야 한다.", async () => { 
-    const res = await supertest(app).get("/api/goods/add/crawling");
-    expect(res.status).toEqual(200);
+describe('json / string을 반환하는 api',()=>{
+    test("/api/goods/add/crawling 경로에 요청했을 때 status code가 200이어야 한다.", async () => { 
+        const res = await supertest(app).get("/api/goods/add/crawling");
+        expect(res.status).toEqual(200);
+    });
 });
-*/
-
-test("/ 경로에 요청했을 때 status code가 200이어야 한다.", async () => { 
-    const res = await supertest(app).get("/");
-    expect(res.status).toEqual(200);
-});
-
