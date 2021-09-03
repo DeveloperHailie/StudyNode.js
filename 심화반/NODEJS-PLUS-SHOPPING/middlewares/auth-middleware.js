addf05226714b76b51d1429701236a3543fd2066
hailie <20181011@sungshin.ac.kr>
2021-09-03 19:27:04 +0900

    mocking하여 auth-middleware 테스트

--- 심화반/NODEJS-PLUS-SHOPPING/middlewares/auth-middleware.js
+++ 심화반/NODEJS-PLUS-SHOPPING/middlewares/auth-middleware.js
@@ -3,13 +3,14 @@ const { User } = require('../models');
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers;
+
     const [tokenType, tokenValue] = authorization.split(' ');
 
     if (tokenType !== 'Bearer') {
         res.status(401).send({
             errorMessage: '로그인 후 사용하세요',
         });
-        return; //next가 호출이 안되게
+        return; 
     }
 
     try {
--- 심화반/NODEJS-PLUS-SHOPPING/server.js
+++ 심화반/NODEJS-PLUS-SHOPPING/server.js
@@ -1,5 +1,5 @@
 const http = require("./app");
-require("./socket"); // 저 코드 실행
+require("./socket"); 
 
 http.listen(8080, () => {
     console.log('서버가 요청을 받을 준비가 됐어요');
