b9c0f5d0d897b17a6f02182ff82968dec491a851
hailie <20181011@sungshin.ac.kr>
2022-05-08 21:25:11 +0900

    [add] pm2 cluster mode에서 소켓 사용 위한 socket 연결 및 이벤트 코드

---
+++ pm2_cluster_mode/socket/socket.js
@@ -0,0 +1,39 @@
+const socketIo = require('socket.io');
+const { createClient } = require("redis");
+const { createAdapter } = require("@socket.io/redis-adapter");
+
+const socketConfig = {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+    allowEIO3: true,
+};
+
+const pubClient = createClient({ url: "redis://localhost:6379" });
+pubClient.on('connect', () => console.log('Connected to pubClient!'));
+const subClient = pubClient.duplicate();
+subClient.on('connect', () => console.log('Connected to subClient!'));
+pubClient.connect();
+subClient.connect();
+
+module.exports = (server, app) => {
+    const io = socketIo(server, socketConfig);
+    app.set('io', io);
+    io.adapter(createAdapter(pubClient, subClient)); // 다중 인스턴스간 데이터 공유
+    io.on('connection', async (socket) => {
+        socket.on('message', (data) => {
+            io.emit('message',{process:process.pid, data}); //모두에게 메시지 전송
+        });
+        socket.on('send room',(data)=>{
+            console.log(process.pid, "send room", data.roomId);
+            io.to(data.roomId).emit("send room",{process:process.pid}); //roomId방에 메시지 전송
+        });
+        socket.on('join room',(data)=>{
+            socket.join(data.roomId); //roomId방에 join
+        })
+        socket.on('disconnect', () => {
+            clearInterval(socket.interval);
+        });
+    });  
+};
